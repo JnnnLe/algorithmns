6711c8477a870989536f5d6d47f345d8b327d477
JnnnLe <jnnnle.sf@gmail.com>
2019-04-23 12:09:51 -0700

    Solved

--- dividingNums.js
+++ dividingNums.js
@@ -7,37 +7,35 @@
 const rangeFn = (left, right) => {
   let nums = [];
   for (let i = left; i <= right; i++) {
-      nums.push(i);
-  }
-  
+    nums.push(i);
+  }  
   return nums;
 };
 
 const recursivelyMod = (num) => {
   let toStringThenSplitNum = num.toString().split('');
   let isTrue = true;
-  for (let i = 0; i <= toStringThenSplitNum; i++) {
-      if (num % parseInt(toStringThenSplitNum[i]) !== 0) {
-          return isTrue = false;
-      }
-  }
-  
-  return isTrue;
+  for (let i = 0; i < toStringThenSplitNum.length; i++) {
+    // console.log(num, toStringThenSplitNum[i])
+    if (num % toStringThenSplitNum[i] !== 0) {
+      isTrue = false;
+      return false;
+    }  
+  } return isTrue
 }
-
+ 
+// console.log(recursivelyMod(123));
 
 const selfDividingNumbers = (left, right) => {
-  let range = rangeFn(left, right);
+  let range = rangeFn(left, right); // returns range array
   let nums = [];
-  for (let i = range[i]; i < range.length; i++) {
-      if (range[i] < 10) {
-          nums.push(range[i])
-      } else if (recursivelyMod(range[i])) {
-          if (true) {
-              nums.push(range[i]);
-          }
-      
-      }
+  for (let i = 0; i < range.length; i++) {
+    if (recursivelyMod(range[i])) {
+      nums.push(range[i]);
+    }
   }
+
   return nums;
 };
+
+selfDividingNumbers(1, 22)
