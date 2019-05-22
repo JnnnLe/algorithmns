c1c7e14bccddf478f1c29cbe23139ad9a44dee52
JnnnLe <jnnnle.sf@gmail.com>
2019-05-21 20:20:08 -0700

    Solution, 6 kyu

--- findPeaks.js
+++ findPeaks.js
@@ -1,30 +1,30 @@
-const pickPeaks = arr => {
-  console.log('Input:', arr)
-  let final = {
-    pos: [],
-    peaks : []
-  };
+// const pickPeaks = arr => {
+//   console.log('Input:', arr)
+//   let final = {
+//     pos: [],
+//     peaks : []
+//   };
 
-  if (arr.length == 0) { return final }
+//   if (arr.length == 0) { return final }
 
-  for (let i = 1; i < arr.length - 1; i++) {
-    let prev = arr[i - 1];
-    let curr = arr[i];
-    let next = arr[i + 1];
+//   for (let i = 1; i < arr.length - 1; i++) {
+//     let prev = arr[i - 1];
+//     let curr = arr[i];
+//     let next = arr[i + 1];
     
-    if (prev == curr && curr == next) {
-      final.peaks.push(curr);
-      final.pos.push(i - 2);
-    }
+//     if (prev == curr && curr == next) {
+//       final.peaks.push(curr);
+//       final.pos.push(i - 2);
+//     }
     
-    if (curr > prev && curr > next) {
-      final.pos.push(i);
-      final.peaks.push(curr);
-    }
-  }
+//     if (curr > prev && curr > next) {
+//       final.pos.push(i);
+//       final.peaks.push(curr);
+//     }
+//   }
 
-  return final;
-}
+//   return final;
+// }
 
 
 // ideas: helper fn to find:
@@ -32,33 +32,26 @@ const pickPeaks = arr => {
   //  push values and find a way to skip iteration there
 
 
-/*
-VALID SOLUTION:
+
+// VALID SOLUTION:
 
 const  pickPeaks = arr => {
   let obj = { pos:[], peaks:[] };
   for (let i = 1; i < arr.length-1; i++) {
-  	//if the current number is higher than previous and higher than the next one, it's the peak
   	if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
   		obj.pos.push(i);
   		obj.peaks.push(arr[i]);
-  	//if current number is higher than the previous and equal to the next
   	} else if (arr[i] > arr[i-1] && arr[i] === arr[i+1]) {
-	  	//save the position and value of this in a temporary variable
 	  	let temp = [];
 	  	temp.push(i);
 	  	temp.push(arr[i]);
 	  	console.log('Temp:', temp);
-	  	//check the next one to see if it's higher than the one after, equal to the next one, or higher than the previous one
 	  	for (let j = i+1; j < arr.length-1; j++) {
-	  	//if it is higher than the next one, return the saved first repeating number
-	  	//3,3,4,5,3
 	  		if (arr[j] > arr[j+1]) {
 	  			obj.pos.push(temp[0]);
 	  			obj.peaks.push(temp[1]);
 	  			break;
 	  		} else if (arr[j] < arr[j+1]) {
-	  			//if the next one is higher, break out
 	  			break;
 	  		} 
 	  	}
@@ -70,4 +63,3 @@ const  pickPeaks = arr => {
 
 console.log(pickPeaks([ 2, 1, 3, 1, 2, 2, 2, 2 ]
   [ 4, 2 ]))
-  */
